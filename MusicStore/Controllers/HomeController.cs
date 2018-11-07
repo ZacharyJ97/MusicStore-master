471f7be788b2faf2d48d1e41fa03a175b8d87b2d
Zachary Ebner <zjebner97@live.com>
2018-11-07 16:29:06 -0500

    Add project files.

---
+++ MusicStore/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Models;
+using MusicStore.Data_Contexts;
+
+namespace MetalMania.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EFDbContext db = new EFDbContext();
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Albums()
+        {
+            // TO-DO: Call GetAlbums() instead of db.Albums
+            ViewBag.Albums = GetAlbums().OrderBy(album => album.Title);
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Search(string searchBy, string searchText)
+        {
+            var albums = GetAlbums();
+
+            if (!String.IsNullOrEmpty(searchText))
+            {
+                List<AlbumListModel> results;
+
+                searchText = searchText.ToLower();
+
+                if (searchBy == "Artist")
+                {
+                    results = albums.Where(x => x.Artist.ToLower().Contains(searchText)).ToList();
+                }
+                else if (searchBy == "Title")
+                {
+                    results = albums.Where(x => x.Title.ToLower().Contains(searchText)).ToList();
+                }
+                else if (searchBy == "Year")
+                {
+                    results = albums.Where(x => x.Year == Int32.Parse(searchText)).ToList();
+                }
+                else
+                {
+                    results = albums.Where(x => x.Title.ToLower().Contains(searchText)
+                                            || x.Artist.ToLower().Contains(searchText))
+                                            .ToList();
+                }
+
+                return View(results);
+            }
+
+            return View(albums.OrderBy(x => x.Year).ToList());
+
+        }
+
+        private IQueryable<AlbumListModel> GetAlbums()
+        {
+            var albums = from alb in db.Albums
+                         join art in db.Artists on alb.ArtistId equals art.ArtistId
+                         select new AlbumListModel
+                         {
+                             AlbumId = alb.AlbumId,
+                             Artist = art.Name,
+                             Title = alb.Title,
+                             Year = alb.Year
+                         };
+
+            return albums;
+        }
+      
+    }
+}
---
+++ MusicStore/Data Contexts/EFDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MusicStore.Models;
+
+namespace MusicStore.Data_Contexts
+{
+    public class EFDbContext : DbContext
+    {
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Artist> Artists { get; set; }
+
+        public EFDbContext() : base("name=musicStoreDbContext")
+        {
+
+        }
+    }
+}
